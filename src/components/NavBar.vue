fd9457a7f49a8cf6722b00532d51f040ad548fb7
Aniket Aryamane <aryamane.aniket@gmail.com>
2024-06-28 23:25:47 +0530

    Adding Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+    <nav
+        class="navbar navbar-expand-lg bg-dark"
+        data-bs-theme="dark"
+    >
+        <div class="container-fluid">
+            <router-link
+                class="navbar-brand"
+                :to="{ name: 'Home' }"
+            >
+                Home
+            </router-link>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+                id="navbarNav"
+                class="collapse navbar-collapse"
+            >
+                <ul class="navbar-nav ms-auto d-flex align-items-center gap-1">
+                    <li class="nav-item">
+                        <router-link
+                            class="nav-link"
+                            :to="{ name: 'Cars' }"
+                        >
+                            Cars
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link
+                            class="nav-link"
+                            :to="{ name: 'About' }"
+                        >
+                            About
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+    color: yellow;
+    background-color: darkslategray;
+    padding: 4px;
+    text-decoration: none !important;
+}
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,17 +1,12 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+</script>
+
 <template>
     <div class="container">
-        <router-link :to="{ name: 'Home' }">Home</router-link>
-        |
-        <router-link :to="{ name: 'Cars' }">Cars</router-link>
-        |
-        <router-link :to="{ name: 'About' }">About</router-link>
+        <NavBar />
         <router-view />
     </div>
 </template>
 
-<style scoped>
-.router-link-exact-active {
-    color: green;
-    text-decoration: none !important;
-}
-</style>
+<style scoped></style>
